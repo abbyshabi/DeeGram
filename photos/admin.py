00236971b78cc7b19005f0fb408273a5153fec8a
abby-shabi <abbyshabi@gmail.com>
2019-03-10 11:17:30 +0300

    added register to admin.py

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Image,Location,Comments,Profile
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Comments)
+admin.site.register(Profile)
