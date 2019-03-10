be12b4bfe9dff430e041a3296ae4ec8c9c669309
abby-shabi <abbyshabi@gmail.com>
2019-03-10 11:24:45 +0300

    index to views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import ImageForm,ProfileForm,CommentForm
+from .models import Image,Profile,User,Comments
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    current_user = request.user
+    images = Image.objects.order_by('-date')
+    profile = Profile.objects.order_by('-last_update')
+     
+    return render(request,'index.html',{"images":images,"profile":profile})
